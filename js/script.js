5b67e91072965b11d69661a593adf2156e6690b4
Lorena Spiller <lorenaspiller@gmail.com>
2021-04-19 20:38:47 +0200

    added data objects in Js, added first part of products section
    also: fixed primary button color and nav-bar links height + main hover

--- js/script.js
+++ js/script.js
@@ -1,1 +1,99 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    shoppingItems: 0,
+    navbar: [
+      {
+        name: 'home',
+        icon: false,
+        position: {
+          header: true,
+          footer: false
+        },
+        dropDown: [
+          'Classic Shop',
+          'Extended Width'
+        ]
+      },
+      {
+        name: 'shop',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'about',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'gallery',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'locations',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'journal',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'contact',
+        icon: false,
+        position: {
+          header: true,
+          footer: true
+        }
+      },
+      {
+        name: 'my account',
+        icon: false,
+        position: {
+          header: true,
+          footer: false
+        }
+      },
+      {
+        name: 'orders',
+        icon: false,
+        position: {
+          header: false,
+          footer: true
+        }
+      },
+      {
+        title: 'shopping-cart',
+        family: 'fas',
+        prefix: 'fa-',
+        icon: true,
+        position: {
+          header: true,
+          footer: true
+        },
+        itemCounter: this.shoppingItems,
+      }
+    ]
+  },
+  methods: {
 
+  }
+
+})
